35023b60f476f13352a20bf7fbf53c7f763ec536
meliketelef <meliketelef0935@gmail.com>
2025-03-17 16:20:17 +0300

    meliketest

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
-import { useState } from 'react';
-import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
-import MainPage from './MainPage';
+import { useState } from "react";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
+import MainPage from "./MainPage";
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
-
+  //aaaa
   if (!isAuthenticated) {
     if (showRegister) {
       return (
-        <RegisterPage 
+        <RegisterPage
           onRegister={() => setIsAuthenticated(true)}
           switchToLogin={() => setShowRegister(false)}
         />
       );
     }
     return (
-      <LoginPage 
+      <LoginPage
         onLogin={() => setIsAuthenticated(true)}
         switchToRegister={() => setShowRegister(true)}
       />
